dac5725b29448366a07f948a2d7b091abba12436
Igor Santos <santosigor868@gmail.com>
2024-05-17 22:12:52 -0300

    Up RUn

---
+++ app.js
@@ -0,0 +1,22 @@
+function tocarSom(e){
+    const codigoLetra = e.keyCode
+    const audio = document.querySelector(`audio[data-key="${codigoLetra}"]`)
+    if(!audio) return
+
+    const tecla = document.querySelector(`div[data-key="${codigoLetra}"]`)
+    tecla.classList.add("tocando")
+    audio.currenTime = 0
+    audio.play()
+    }
+
+    function removeClasse(e){
+        if(e.propertyName !== 'transform') return
+        e.target.classList.remove('tocando')
+    }
+
+    const todasTeclas= Array.from(document.querySelectorAll('.tecla'))
+    todasTeclas.forEach(tecla => tecla.addEventListener('transitionend', removeClasse))
+    window.addEventListener("keydown",tocarSom)
+
+
+window.addEventListener("keydown", tocarSom)
